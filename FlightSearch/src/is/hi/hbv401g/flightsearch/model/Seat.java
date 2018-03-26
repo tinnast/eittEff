c27e5567f13f86f91a48a4628afd857915a959f4
Mimoza <mhr4@hi.is>
2018-03-26 10:02:26 +0000

    Klasin Airline

--- FlightSearch/src/is/hi/hbv401g/flightsearch/model/Seat.java
+++ FlightSearch/src/is/hi/hbv401g/flightsearch/model/Seat.java
@@ -7,6 +7,34 @@ package is.hi.hbv401g.flightsearch.model;
  * @author Tinna Sturludóttir			300589-2439 <tis12@hi.is>
  */
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+=======
+package is.hi.hbv401g.flightsearch.model;
+
+
+public class Seat {
+
+	public static void main(String[]args) {
+		
+		private int price;
+
+		private String Class;
+
+		private Passenger passenger; //import from the project
+
+		private boolean entertainment;
+
+		private String electricalConnections;
+
+		private String luggage;
+>>>>>>> Stashed changes
+
+
+<<<<<<< Updated upstream
+
+>>>>>>> Stashed changes
 public class Seat {
 	
 	private int price;
@@ -27,10 +55,57 @@ public class Seat {
 		return seatClass;
 	}
 
+<<<<<<< Updated upstream
 	public int getSeatNumber() {
 		return seatNumber;
 
 	}
+=======
+=======
+		private int seatNumber;
+		
+		/**
+		 * @return price
+		 */
+		public int getPrice() {
+
+		return price;
+
+		}
+
+		/**
+		 * @return Class
+		 */
+		public String getClass() {
+>>>>>>> Stashed changes
+
+		public String getSeatClass() {
+			return seatClass;
+
+		}
+<<<<<<< Updated upstream
+
+		public int getSeatNumber() {
+			return seatNumber;
+=======
+		
+		
+		/**
+		 * @return SeatNo
+		 */
+		public int getSeatNo() {
+
+		return SeatNo;
+>>>>>>> Stashed changes
+
+		}
+		
+		/**
+		 * @return Passenger
+		 */
+		public Passenger getPassenger(){
+			return passenger;
+>>>>>>> Stashed changes
 
 	public Passenger getPassenger() {
 		return passenger;
