521856561987fb880605395c5c6d438985474e1f
Tinna <tis12@hi.is>
2018-03-26 13:05:33 +0000

    bla

--- FlightSearch/test/is/hi/hbv401g/flightsearch/controller/CorrectList.java
+++ FlightSearch/test/is/hi/hbv401g/flightsearch/controller/CorrectList.java
@@ -7,10 +7,71 @@
  */
 package is.hi.hbv401g.flightsearch.controller;
 
+import java.util.List;
+
+import javax.management.Query;
+
+import is.hi.hbv401g.flightsearch.model.Booking;
+import is.hi.hbv401g.flightsearch.model.Flight;
+
 /**
  * @author tinna
  *
  */
-public class CorrectList implements DBManager{
+public class CorrectList implements DBManager {
 
+	/* (non-Javadoc)
+	 * @see is.hi.hbv401g.flightsearch.controller.DBManager#searchByQuery(is.hi.hbv401g.flightsearch.controller.Query)
+	 */
+	@Override
+	public List<Flight> searchByQuery(Query q) {
+		List<Flight> flightList = new ArrayList<Flight>();
+		return flightList;
+	}
+
+	/* (non-Javadoc)
+	 * @see is.hi.hbv401g.flightsearch.controller.DBManager#getLocations()
+	 */
+	@Override
+	public List<String> getLocations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see is.hi.hbv401g.flightsearch.controller.DBManager#getPopularLocations()
+	 */
+	@Override
+	public List<String> getPopularLocations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see is.hi.hbv401g.flightsearch.controller.DBManager#addBooking(is.hi.hbv401g.flightsearch.model.Booking)
+	 */
+	@Override
+	public void addBooking(Booking bokking) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see is.hi.hbv401g.flightsearch.controller.DBManager#searchForBooking(int)
+	 */
+	@Override
+	public Booking searchForBooking(int bookNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see is.hi.hbv401g.flightsearch.controller.DBManager#searchByQuery(is.hi.hbv401g.flightsearch.controller.Query)
+	 */
+	@Override
+	public List<Flight> searchByQuery(is.hi.hbv401g.flightsearch.controller.Query q) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
