2dfac4456c63ed880726c604964d79100cfc92f8
Karl Aldinsteinn <clevern@me.com>
2018-03-26 13:08:47 +0000

    Interface is more interfacey

--- FlightSearch/test/is/hi/hbv401g/flightsearch/controller/FlightMock.java
+++ FlightSearch/test/is/hi/hbv401g/flightsearch/controller/FlightMock.java
@@ -11,7 +11,7 @@ import java.sql.Date;
 import java.util.ArrayList;
 
 
-public class FlightMock {
+public class FlightMock implements Flight {
 	
 	private String departure;
 	private String arrival;
