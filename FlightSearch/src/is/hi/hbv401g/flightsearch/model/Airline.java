4dbd9a42d9964097606d1a7d5ba173316e2df4f9
Tinna <tis12@hi.is>
2018-03-26 12:58:40 +0000

    Auto stash before merge of "master" and "origin/master"

--- FlightSearch/src/is/hi/hbv401g/flightsearch/model/Airline.java
+++ FlightSearch/src/is/hi/hbv401g/flightsearch/model/Airline.java
@@ -1,4 +1,4 @@
-
+/*
  * @author Aníta Kristjánsdóttir 		190592-2949 <ank16@hi.is>
  * @author Karl James Pestka 			101083-2689 	<kjp3@hi.is>
  * @author Mimoza Herta Róbertsdóttir 	310194-3289 <mhr4@hi.is>
@@ -13,19 +13,20 @@ public class Airline {
 	private double avgRating;
 	private int totalRatings;
 	  
-	public Airline(String airlineName, double avgRating, int totalRatings) { //construnctor of Airline class
-	this.airlineName=airlineName;
-	this.avgRating=avgRating;
-	this.totalRatings=totalRatings
+public Airline(String airlineName, double avgRating, int totalRatings) { //construnctor of Airline class
+	this.airlineName = airlineName;
+	this.avgRating = avgRating;
+	this.totalRatings = totalRatings;
 	}
 	  
-	public addRating(int rating) {
+public addRating(int rating) {
 	// Calculaing the average rating
-	this.avgRating=((this.avgRating*this.totalRatings)+rating)/(this.totalRatings+1);
-	this.totalRatings=this.totalRatings+1 // incrementing the total ratings
+	this.avgRating = ((this.avgRating * this.totalRatings) + rating) / (this.totalRatings + 1);
+	this.totalRatings = this.totalRatings + 1; // incrementing the total ratings
+	return avgRating;
 	}
 	  
-	public double getRating() {
+public double getRating() {
 	return this.avgRating;
 	}
 }
