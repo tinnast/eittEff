96c7f56ac7cc0868947bb3799c6b3ace218b9680
Tinna <tis12@hi.is>
2018-04-10 12:05:04 +0000

    Auto stash before merge of "master" and "origin/master"
    View vinna

--- FlightSearch/src/is/hi/hbv401g/flightsearch/view/FlightSearchView1.java
+++ FlightSearch/src/is/hi/hbv401g/flightsearch/view/FlightSearchView1.java
@@ -1,9 +1,9 @@
 /**
  * Group 1F
- * @author Aníta Kristjánsdóttir 		190592-2949 <ank16@hi.is>
+ * @author Anï¿½ta Kristjï¿½nsdï¿½ttir 		190592-2949 <ank16@hi.is>
  * @author Karl James Pestka 			101083-2689 	<kjp3@hi.is>
- * @author Mimoza Herta Róbertsdóttir 	310194-3289 <mhr4@hi.is>
- * @author Tinna Sturludóttir			300589-2439 <tis12@hi.is>
+ * @author Mimoza Herta Rï¿½bertsdï¿½ttir 	310194-3289 <mhr4@hi.is>
+ * @author Tinna Sturludï¿½ttir			300589-2439 <tis12@hi.is>
  * he
  */
 package is.hi.hbv401g.flightsearch.view;
@@ -13,15 +13,27 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import com.toedter.calendar.JDateChooser;
+
+import is.hi.hbv401g.flightsearch.controller.DBManager;
+import is.hi.hbv401g.flightsearch.controller.EmptySearchList;
+import is.hi.hbv401g.flightsearch.controller.FlightSearchController;
+import is.hi.hbv401g.flightsearch.model.Flight;
+
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
 import javax.swing.JSeparator;
+import javax.swing.JTextPane;
 
 /**
  * @author tinna
@@ -35,6 +47,10 @@ public class FlightSearchView1 extends JFrame {
 	private JButton btnSearch;
 	private static final long serialVersionUID = 1L;
 	private JTextField txtBookingNumber;
+	private JDateChooser dateChooserFrom;
+	private JDateChooser dateChooserTo;
+	private JTextField txtPassCount;
+	private JTextPane textPane;
 
 	/**
 	 * Launch the application.
@@ -82,26 +98,46 @@ public class FlightSearchView1 extends JFrame {
 		btnSearch = new JButton("Find me a flight!");
 		btnSearch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				String from, to, date1Str, date2Str;
+				int passCount;
+				
+				// NÃ¡ Ã­ dagsettningar
+				java.util.Date date1 = dateChooserFrom.getDate();
+				java.util.Date date2 = dateChooserTo.getDate();
+				// Formatta dagsetningu
+				java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("dd.MM.yyyy");
+							
+				from = txtFrom.getText();
+				to = txtTo.getText();
+				date1Str = DateFormat.getDateInstance().format(date1);
+				date2Str = DateFormat.getDateInstance().format(date2);
+				passCount = Integer.parseInt(txtPassCount.getText());
+				// Bara til aÃ° testa Ãºtkomuna
+				textPane.setText(from + " " + to + " " + sdf.format(date1) + " " + sdf.format(date2) + " " + passCount);
+				
+				
+				
+				/*
+				FlightSearchController mySearch = new FlightSearchController(null);
+				mySearch.search(from, to, date1Str, date2Str, passCount);
+				*/
+				
 			}
 		});
 		btnSearch.setBounds(160, 343, 234, 51);
 		contentPane.add(btnSearch);
 		
-		JDateChooser dateChooserFrom = new JDateChooser();
+		dateChooserFrom = new JDateChooser();
 		dateChooserFrom.setToolTipText("select date");
 		dateChooserFrom.setDateFormatString("dd-MM-yyyy");
 		dateChooserFrom.setBounds(50, 106, 125, 26);
 		contentPane.add(dateChooserFrom);
 		
-		JDateChooser dateChooserTo = new JDateChooser();
+		dateChooserTo = new JDateChooser();
 		dateChooserTo.setToolTipText("select date");
 		dateChooserTo.setBounds(328, 106, 125, 26);
 		contentPane.add(dateChooserTo);
-		
-		JLabel lblTopDestinations = new JLabel("Top Destinations");
-		lblTopDestinations.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTopDestinations.setBounds(179, 174, 185, 20);
-		contentPane.add(lblTopDestinations);
 		
 		JLabel lblSeeMyBooking = new JLabel("View my booking");
 		lblSeeMyBooking.setBounds(35, 432, 105, 16);
@@ -120,5 +156,18 @@ public class FlightSearchView1 extends JFrame {
 		JSeparator separator = new JSeparator();
 		separator.setBounds(25, 407, 492, 9);
 		contentPane.add(separator);
+		
+		txtPassCount = new JTextField();
+		txtPassCount.setColumns(10);
+		txtPassCount.setBounds(149, 145, 26, 26);
+		contentPane.add(txtPassCount);
+		
+		textPane = new JTextPane();
+		textPane.setBounds(50, 196, 434, 126);
+		contentPane.add(textPane);
+		
+		JLabel lblNoOfPassengers = new JLabel("No. of passengers");
+		lblNoOfPassengers.setBounds(40, 150, 119, 20);
+		contentPane.add(lblNoOfPassengers);
 	}
 }
